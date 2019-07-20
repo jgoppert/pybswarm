bae096554ca1ecb47129a47a866c22e6c33f3f5a
James Goppert <james.goppert@gmail.com>
2019-07-19 20:30:21 -0400

    Update hooked on a feeling.

--- scripts/hookedonafeeling.py
+++ scripts/hookedonafeeling.py
@@ -35,7 +35,7 @@ def scale_formation(form, scale):
 rotation_order = [4, 3, 2, 5, 0, 1]
 
 # scaling for formation
-form_scale = np.array([2, 2, 1])
+form_scale = np.array([1.5, 1.5, 1])
 
 # the takeoff formation
 formTakeoff = np.array([
@@ -47,6 +47,7 @@ formTakeoff = np.array([
     [0.5, 1, 0],
 ]).T
 n_drones = formTakeoff.shape[1]
+#%%
 
 plot_formation(formTakeoff, 'takeoff')
 points = []
@@ -56,66 +57,115 @@ for i_drone in rotation_order:
 formCircle = scale_formation(np.array(points).T, form_scale)
 plot_formation(formCircle, 'circle')
 
+#%%
 points = []
 for i_drone in rotation_order:
     theta = i_drone*2*np.pi/n_drones
     if i_drone % 2 == 0:
-        points.append([0.5*np.cos(theta), 0.5*np.sin(theta), 0])
+        points.append([0.5*np.cos(theta), 0.5*np.sin(theta), -0.5])
     else:
-        points.append([0.25*np.cos(theta), 0.25*np.sin(theta), 0])
+        points.append([0.5*np.cos(theta), 0.5*np.sin(theta), 0.5])
 formTriangle = scale_formation(np.array(points).T, form_scale)
 plot_formation(formTriangle, 'triangle')
 
+#%%
 
 class Geometry:
 
+    rgb = {
+        'black': [0, 0, 0],
+        'gold': [255, 100, 15],
+        'red': [255, 0, 0],
+        'green': [0, 255, 0],
+        'blue': [0, 0, 255],
+        'white': [255, 255, 255]
+    }
+
     def __init__(self):
-        self.T = []
         self.waypoints = [formTakeoff]
+        self.T = []
+        self.delays = []
+        self.colors = []
 
     @staticmethod
     def sin_z(form, t):
         new_form = np.array(form)
         n_drones = form.shape[1]
         for i in rotation_order:
-            new_form[2, i] = 0.4 * np.sin(t + i*2*np.pi/n_drones)
+            new_form[2, i] = 0.5*np.sin(t + i*2*np.pi/n_drones)
         return new_form
 
-    def sin_wave(self, form, n, duration):
+    def sin_wave(self, form, n, duration, color):
         for t in np.linspace(0, duration, n):
             formSin = self.sin_z(form, t*np.pi/4)
             self.waypoints.append(formSin)
             self.T.append(duration/n)
+            self.colors.append(self.rgb[color])
+            n_drones = form.shape[1]
+            self.delays.append(np.zeros(n_drones).tolist())
 
-    def spiral(self, form, z, n, duration):
+    def spiral(self, form, z, n, duration, color):
         for t in np.linspace(0, 1, n):
             rot_form = formation.rotate_points_z(form, t*2*np.pi)
             shift = np.array([[0, 0, z*np.sin(t)]]).T
             self.waypoints.append(rot_form + shift)
             self.T.append(duration/n)
+            self.colors.append(self.rgb[color])
+            n_drones = form.shape[1]
+            self.delays.append(np.zeros(n_drones).tolist())
 
-    def rotate(self, form, n, duration):
+    def rotate(self, form, n, duration, color):
         for t in np.linspace(0, 1, n):
             rot_form = formation.rotate_points_z(form, t*2*np.pi)
             self.waypoints.append(rot_form)
             self.T.append(duration/n)
+            self.colors.append(self.rgb[color])
+            n_drones = form.shape[1]
+            self.delays.append(np.zeros(n_drones).tolist())
 
-    def goto(self, form, duration):
+    def goto(self, form, duration, color):
         self.waypoints.append(form)
         self.T.append(duration)
+        self.colors.append(self.rgb[color])
+        n_drones = form.shape[1]
+        self.delays.append(np.zeros(n_drones).tolist())
+    
+    def plan_trajectory(self):
+        trajectories = []
+        origin = np.array([1.5, 2, 2])
+
+        for drone in range(waypoints.shape[2]):
+            pos_wp = waypoints[:, :, drone] + origin
+            yaw_wp = np.zeros((pos_wp.shape[0], 1))
+            traj = tgen.min_deriv_4d(4, 
+                np.hstack([pos_wp, yaw_wp]), T, stop=False)
+            trajectories.append(traj)
+
+        traj_json = tgen.trajectories_to_json(trajectories)
+        data = {}
+        for key in traj_json.keys():
+            data[key] = {
+                'trajectory': traj_json[key],
+                'T': T,
+                'color': g.colors,
+                'delay': [d[key] for d in g.delays]
+            }
+        data['repeat'] = 3
+        assert len(trajectories) < 32
+        return trajectories, data
 
 # create trajectory waypoints
 g = Geometry()
 #ooga chaka
-g.sin_wave(form=formTakeoff, n=4, duration=9)
+g.sin_wave(form=formTakeoff, n=4, duration=9, color='gold')
 #IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII gotta feeling
-g.goto(form=formCircle, duration=2)
-g.spiral(form=formCircle, z=1, n=7, duration=12)
+g.goto(form=formCircle, duration=2, color='red')
+g.spiral(form=formCircle, z=1, n=7, duration=12, color='red')
 #less popular part of chorus
-g.goto(form=formCircle, duration=2)
-g.rotate(form=formTriangle, n=9, duration=17)
+g.goto(form=formCircle, duration=2, color='green')
+g.rotate(form=formTriangle, n=9, duration=17, color='white')
 #before alcohol
-g.rotate(form=formCircle, n=8, duration=14)
+g.rotate(form=formCircle, n=8, duration=14, color='blue')
 
 #g.rotate(form=formCircle, n=7, duration=20)
 #g.rotate(form=formTriangle, n=4, duration=12)
@@ -124,39 +174,20 @@ g.rotate(form=formCircle, n=8, duration=14)
 #g.spiral(form=formCircle, z=1, n=7, duration=28)
 #g.goto(form=formCircle, duration=2)
 #g.rotate(form=formTriangle, n=5, duration=18)
-g.goto(formTakeoff, 2)
+g.goto(formTakeoff, 2, color='green')
 
 ##
 #18 secs for next segment of song
 ##
 
-# get waypoints and durations
-waypoints = np.array(g.waypoints)
-T = g.T
-
-
-plt.figure()
-ax = plt.axes(projection='3d')
-for point in range(waypoints.shape[2]):
-    ax.plot3D(waypoints[:, 0, point], waypoints[:, 1, point], waypoints[:, 2, point], '-')
-    ax.view_init(azim=45, elev=40)
-plt.title('waypoints')
-plt.show()
-
 #%% plan trajectories
-trajectories = []
-origin = np.array([1.5, 2, 2])
+trajectories, data = g.plan_trajectory()
 
-for drone in range(waypoints.shape[2]):
-    pos_wp = waypoints[:, :, drone] + origin
-    yaw_wp = np.zeros((pos_wp.shape[0], 1))
-    traj = tgen.min_deriv_4d(4, 
-        np.hstack([pos_wp, yaw_wp]), T, stop=False)
-    trajectories.append(traj)
+with open('scripts/data/HookedOnAFeeling.json', 'w') as f:
+    json.dump(data, f)
 
-tgen.trajectories_to_json(trajectories, 'scripts/data/HookedOnAFeeling.json')
 tgen.plot_trajectories(trajectories)
-tgen.animate_trajectories('HookedOnAFeeling.mp4', trajectories, 1)
+#tgen.animate_trajectories('hooked.mp4', trajectories, 1)
 
 #%%
 plt.figure()
@@ -169,11 +200,20 @@ tgen.plot_trajectories_magnitudes(trajectories)
 plt.show()
 
 #%%
-print('number of segments', len(traj.coef_array()))
+print('number of segments', len(trajectories[0].coef_array()))
 #%%
 plt.figure()
 plt.title('durations')
-plt.bar(range(len(T)), T)
+plt.bar(range(len(g.T)), g.T)
 plt.show()
 
-## jgoppert, tkapoor, fishabh
+
+#%%
+
+
+
+
+
+
+
+
